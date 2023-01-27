675a503322d51e3b988228cc80f230257849aa37
Alexander Weinberg <weinberga@Alexanders-MacBook-Air.local>
2023-01-27 14:55:06 -0600

    ran some tests on the inner loop

--- pset1/Code/q4.jl
+++ pset1/Code/q4.jl
@@ -3,12 +3,12 @@
 # ================================ #
 # LOAD PACKAGES
 # ================================ #
-using CSV, DataFrames, GLM, Optim, Random, LinearAlgebra
+using CSV, DataFrames, GLM, Optim, Random, LinearAlgebra, Statistics
 
 # PARAM
 T = 100
 J = 6
-Ns = 50
+Ns = 10000
 
 # NUMPARAM
 delta_init      = ones(J,1) ./ J
@@ -34,16 +34,10 @@ function compute_share_ijt(δ_jt, xtilde_jt, Gamma, v_i)
     # x_jt is product characteristics
     # Γ is the correlation matrix
     # v_l is the individual preference shock
-
-    top                     = zeros(J)
-
-    for j in 1:J
-        aux = xtilde_jt[j] * Gamma * v_i
-        top[j] = exp(δ_jt[j] + aux[1])
-    end
-    bottom = sum(top) + 1
-
-    share_ijt = top / bottom
+    inner           = xtilde_jt * Gamma * v_i 
+    top             = exp.(δ_jt .+ inner)
+    bottom          = sum(top) + 1
+    share_ijt       = top / bottom
     return share_ijt
 end
 
@@ -52,19 +46,24 @@ end
 # ================================ #
 function compute_share_jt(δ_jt, xtilde_jt, Gamma)
 
+    # initialize
     share_ijt = zeros(Ns,J)
     share_jt  = zeros(1,J)
 
-    for i in 1:Ns
-        # Draw shocks
-        vi = randn(1,2)
+    # draw shocks
+    v_mat = randn(2,Ns)
+    v_mat = zeros(2,Ns)
 
+    for i in 1:Ns
+        # Draw shock i
+        v_i = v_mat[:,i]
+        
         # Compute individual shares 
         output = compute_share_ijt(δ_jt, xtilde_jt, Gamma, v_i)
         share_ijt[i,:] = transpose(output)
-
     end
 
+    # compute share
     share_jt = sum(share_ijt, dims=1) ./ Ns
 
     return share_jt
@@ -76,21 +75,22 @@ end
 function update_delta(delta_old_jt, s_jt_obs, xtilde_jt, Gamma)
 
     # Given delta compute predicted shares
-    s_jt_old = compute_share_jt(delta_old_jt, xtilde_jt, Gamma)
-    s_jt_old = reshape(s_jt_old, J, 1)
+    s_jt_old        = compute_share_jt(delta_old_jt, xtilde_jt, Gamma)
 
+    # ensure dimensions are correct
+    s_jt_old        = reshape(s_jt_old, J, 1)
     s_jt_obs        = reshape(s_jt_obs, J, 1)
     delta_old_jt    = reshape(delta_old_jt, J, 1)
     
     delta_new_jt = delta_old_jt + log.(s_jt_obs) - log.(s_jt_old)
 
-    return delta_new_jt, s_jt_old
+    return delta_new_jt
 end
 
 function solve_delta(delta_init, s_jt_obs, xtilde_jt, Gamma)
 
     # Initialize 
-    diff_delta   = 1e10
+    diff_delta   = 100
     delta_old_jt = delta_init
     delta_new_jt = similar(delta_old_jt)
 
@@ -101,31 +101,25 @@ function solve_delta(delta_init, s_jt_obs, xtilde_jt, Gamma)
         # update iteration count 
         iter_d += 1
         # update delta
-        delta_new_jt, s_jt_old = update_delta(delta_old_jt, s_jt_obs, xtilde_jt, Gamma)
+        delta_new_jt = update_delta(delta_old_jt, s_jt_obs, xtilde_jt, Gamma)
 
         diff_delta = maximum(abs.(delta_old_jt - delta_new_jt))
         delta_old_jt = delta_new_jt
 
         if diff_delta < tol_delta
-            println("---\nSolved for δ_jt in $iter_d iterations. Difference is $diff_delta")
+            println("Solved for δ_jt in $iter_d iterations. Difference is $diff_delta")
             break
         end
 
         # UPDATE
-        if iter_d % 100 == 0
-            print("Current iteration is $iter_d. Diff = $diff_delta.\n")
-            # println("Delta new is ", delta_new_jt)
-            println("Predicted shares are ")
-            println(round.(s_jt_old, digits=4))
-            println(s_jt_obs)
-            println("Observed shares ^ ")
-  
+        if iter_d % 200 == 0
+            print("Current iteration is $iter_d. Diff = $diff_delta.\n")  
         end
  
     end
 
     if iter_d == max_iter_delta
-        println("---\nDid not solve for delta. Difference is $diff_delta.\n---\n")
+        println("Did not solve for delta. Difference is $diff_delta.\n---\n")
     end
 
     return delta_new_jt
@@ -142,6 +136,7 @@ function do_delta_loop(df, Gamma, delta_init)
 
     for t in 1:T
         # market index
+        println("---\nNow solving market $t/$T")
         market_idx = (df[:,1] .== t)
 
         # Get the data
@@ -162,18 +157,25 @@ end
 
 
 
-# # test function
+# # test function. fix gamma. 
 Gamma = [1 0; 1 1]
+# println("--\nmarket 1....T")
+t = 1
+market_idx = (df[:,1] .== t)
 
+# Get the data
+s_jt_obs = df[market_idx, 3]
 
-δ_jt     = ones(J,1)
+p_jt = df[market_idx, 4]
+x_jt = df[market_idx, 5]
+xtilde_jt = [p_jt x_jt]
+delta_init = similar(delta_init)
 
-println("--\nmarket 1..T")
-do_delta_loop(df, Gamma, delta_init)
+s_jt_obs[1] = .20
 
+abc = solve_delta(delta_init, s_jt_obs, xtilde_jt, Gamma)
 
-
-
+for t = 
 
 
 # # # Objective GMM
