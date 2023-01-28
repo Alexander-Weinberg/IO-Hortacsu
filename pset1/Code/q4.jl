8052cf6e45607fb34eaaf9203ee17f64ce8a858e
m0nbar <123611309+m0nbar@users.noreply.github.com>
2023-01-27 19:51:45 -0600

    We wrote the GMM objective function

--- pset1/Code/q4.jl
+++ pset1/Code/q4.jl
@@ -1,14 +1,15 @@
 # cd("/Users/Monica/Documents/UChicago/Classes/Industrial Organization II")
+cd("/Users/Monica/Downloads/IO-Hortacsu-main/pset1")
 
 # ================================ #
 # LOAD PACKAGES
 # ================================ #
-using CSV, DataFrames, GLM, Optim, Random, LinearAlgebra, Statistics
+using CSV, DataFrames, GLM, Optim, Random, LinearAlgebra, Statistics, Econometrics, Optim, BlackBoxOptim
 
 # PARAM
 T = 100
 J = 6
-Ns = 10000
+Ns = 50
 
 # NUMPARAM
 delta_init      = ones(J,1) ./ J
@@ -107,7 +108,7 @@ function solve_delta(delta_init, s_jt_obs, xtilde_jt, Gamma)
         delta_old_jt = delta_new_jt
 
         if diff_delta < tol_delta
-            println("Solved for δ_jt in $iter_d iterations. Difference is $diff_delta")
+            # println("Solved for δ_jt in $iter_d iterations. Difference is $diff_delta")
             break
         end
 
@@ -136,7 +137,7 @@ function do_delta_loop(df, Gamma, delta_init)
 
     for t in 1:T
         # market index
-        println("---\nNow solving market $t/$T")
+        # println("---\nNow solving market $t/$T")
         market_idx = (df[:,1] .== t)
 
         # Get the data
@@ -149,15 +150,78 @@ function do_delta_loop(df, Gamma, delta_init)
         # solve for delta_jt
         delt_mat[t, :] = transpose(solve_delta(delta_init, s_jt_obs, xtilde_jt, Gamma))
     end
-    return delt_mat
+    delt_vec = vec(transpose(delt_mat))
+    return delt_vec
 end
 
+function do_GMM_objective(Gamma_vec)
+    Gamma_11 = Gamma_vec[1]
+    Gamma_21 = Gamma_vec[2]
+    Gamma_22 = Gamma_vec[3]
+    Gamma = [Gamma_11 0.0; Gamma_21 Gamma_22]
+    data = df[:,4:11]
+    println("hi1")
+    delta_jt  = do_delta_loop(df,Gamma,delta_init)
+    println("hi2")
+    println(size(delta_jt))
+    data[!,"delta_jt"] = delta_jt
+    print("hi3")
+    # 2sls = 
+    model = fit(EconometricModel, # package specific type
+            @formula(delta_jt  ~ x + (p ~ z1 + z2 + z3 + z4 + z5 + z6)), # @formula(lhs ~ rhs)
+            data # a table
+            )
+    ξ_jt = residuals(model)
+    println("hi4")
+    Z = Matrix(df[:,6:11])
+    Ω = inv(transpose(Z)*Z)
+    objective = (transpose(ξ_jt)*Z)*Ω*(transpose(Z)*ξ_jt) 
+    println("hi5")  
+    return objective
+end
 
+# function add_df_to_GMM(Gamma)
+#     objective_df = do_GMM_objective(Gamma,delta_init,df)
+#     return objective_df
+# end
 
+function do_GMM(Gamma_init)
+    Sol = optimize(do_GMM_objective,Gamma_init)
+    return Sol
+end
 
+Gamma_vec = [100,1.2,1.7]
+
+sol = optimize(do_GMM_objective, Gamma_vec, Optim.Options(show_trace=true, iterations = 100, g_tol = 1e-12), LBFGS())
+Optim.minimizer(sol)
+# optimize(do_GMM_objective(Gamma),Gamma_init,LBFGS())
 
 
 # # test function. fix gamma. 
-Gamma = [1 0; 1 1]
-# println("--\nmarket 1....T")
-do_delta_loop(df, Gamma, delta_init)
+# Gamma_init = [1, 1, 0, 1]
+# Gamma = [1 0; 1 1]
+# # println("--\nmarket 1....T")
+# t = 1
+# market_idx = (df[:,1] .== t)
+
+# # Get the data
+# s_jt_obs = df[market_idx, 3]
+
+# p_jt = df[market_idx, 4]
+# x_jt = df[market_idx, 5]
+# xtilde_jt = [p_jt x_jt]
+# delta_init = similar(delta_init)
+
+# s_jt_obs[1] = .20
+
+# abc = solve_delta(delta_init, s_jt_obs, xtilde_jt, Gamma)
+
+# delt_vec = do_delta_loop(df,Gamma,delta_init)
+
+
+
+# do_GMM(Gamma_init)
+
+
+
+
